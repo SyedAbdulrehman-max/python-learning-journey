eded31d90bd6986ca0afe201935a2b5dcfbc7a4d
Syed_Abdulrehman <abdulrehmansyed347@gmail.com>
2025-08-13 10:26:52 -0700

    correct

--- python/chapter 9 practise_set/01_problem.py
+++ python/chapter 9 practise_set/01_problem.py
@@ -1,7 +1,7 @@
 #write a program to read the text from a given file'poems.txt' 
 # and find out whether it contains the word 'twinkle'
 
-with open("problem_1.txt") as word :
+with open("E:\python-learning-journey\python\chapter 9 practise_set/problem_1.txt") as word :
     contant = word.read() # Read once and store in a variable
 
     if "twinkle" in contant:
